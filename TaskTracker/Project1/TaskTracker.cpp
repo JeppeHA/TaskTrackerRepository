8f1e0c06db7de1a51d700faff0d8bb7777692262
Jeppe <144018621+JeppeHA@users.noreply.github.com>
2024-12-31 00:37:32 +0100

    Json parser (0.3)
    Now checks if there is anything in the file if not add an json array (still need to figure out how to add the object inside it
    
    reads thourgh the file to update the index of the new task

--- TaskTracker/Project1/TaskTracker.cpp
+++ TaskTracker/Project1/TaskTracker.cpp
@@ -3,12 +3,34 @@
 #include <ctime>
 #include "json_parser.h"
 #include <string>
+#include <vector>
+#include <filesystem>
 
 using namespace std;
 
 string fileName = "TaskTracker.json";
 
 fstream file;
+
+void Welcome();
+
+void CreateJsonArray() {
+	
+	ifstream in_file(fileName, ios::binary);
+	in_file.seekg(0, ios::end);
+	int file_size = in_file.tellg();
+
+	if (file_size == 0) {
+
+		ofstream MyFile(fileName);
+		MyFile << "[" << endl;
+		MyFile << "" << endl;
+		MyFile << "]" << endl;
+		MyFile.close();
+	}
+}
+
+
 
 void createTask() {
 	
@@ -19,19 +41,43 @@ void createTask() {
 	ifstream MyReadFile(fileName);
 
 	string line;
+	vector<int> ids;
+	
+	int id = 0;
 
 
-	// Use a while loop together with the getline() function to read the file line by line
 	while (getline(MyReadFile, line)) {
 		if (line.find("id") != std::string::npos) {
+
+			char charId = line[line.length() - 3];
+			string stringId(1, charId);
+			int currentId = stoi(stringId);
+			ids.push_back(currentId);
+
+
+			cout << stringId << endl;
 			noTasks = false;
+
+		
 		}
 	}
 
-	cout << "Enter task name: ";
+
+	
+	if (!noTasks) {
+		// Find the highest ID and increment it
+		for (int currentId : ids) {
+			if (currentId > id) {
+				id = currentId;
+			}
+		}
+		id += 1; 
+	}
+	
 	string taskName;
+	cout << "Enter task name: ";
 	getline(cin, taskName);
-	int id = 1;
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	string description;
 	cout << "Enter task description name: ";
 	getline(cin, description);
@@ -49,7 +95,9 @@ void createTask() {
 	localtime_s(&timeInfo, &createdAt); // Safer version of localtime
 	strftime(formattedTime, sizeof(formattedTime), "%Y-%m-%d %H:%M:%S", &timeInfo);
 
-
+	//TO DO find the end of the array ]
+	//Insert the new object there 
+	//Add ] back
 
 	//Opens the file and appends it
 	file.open(fileName, ios::app);//append
@@ -69,28 +117,59 @@ void createTask() {
 		file << "}";
 		
 		file.close();
+		Welcome();
 	}
 
 }
 
+void ReadTasks() {
+
+	ifstream MyReadFile(fileName);
+
+	string line;
+
+	while (getline(MyReadFile, line)) {
+		cout << line << endl;
+	}
+	MyReadFile.close();
+	Welcome();
+
+}
+
+void TakeInput() {
+	string input = "";
+	cin >> input;
+	cout << "----------------------------" << endl;
+	if (input == "a") {
+		createTask();
+	}
+	else if (input == "b") {
+		ReadTasks();
+	}
+	else if (input == "c") {
+		cout << "Update task" << endl;
+	}
+}
+
+void Welcome() {
+
+	
+
+	cout << "----------------------------" << endl;
+	cout << "Welcome to your task manager" << endl;
+	cout << "What would you like to do:" << endl;
+	cout << "a) Create a task" << endl;
+	cout << "b) See all tasks" << endl;
+	cout << "c) Update a task" << endl;
+
+	TakeInput();
+}
+
 int main() {
 	
+	CreateJsonArray();
+	Welcome();
 	
-	createTask();
-	
-	//Create JSON file if it does not exists
-	/*
-	file.open(fileName, ios::out);//write
-	if (file.is_open()) {
-		file << "test";
-		file.close();
-	}
-	*/
-
-	
-	
-
-
 
 	return 0;
 }
